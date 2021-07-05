1a953c832db4c75ad29ec3583bc9e9fc6c85922e
Erick Contreras <eri89--@hotmail.com>
2021-07-05 18:58:25 -0500

    Fix some linters

--- js/books.js
+++ js/books.js
@@ -1,80 +1,77 @@
 function localStorageAvailable() {
   try {
-    storage = window['localStorage'];
-    var x = '__storage_test__';
+    const storage = window.localStorage;
+    const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
-  }
-  catch(e) {
+  } catch (e) {
     return e instanceof DOMException && (
-        // everything except Firefox
-        e.code === 22 ||
-        // Firefox
-        e.code === 1014 ||
-        // test name field too, because code might not be present
-        // everything except Firefox
-        e.name === 'QuotaExceededError' ||
-        // Firefox
-        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-        // acknowledge QuotaExceededError only if there's something already stored
-        (storage && storage.length !== 0);
+      // everything except Firefox
+      e.code === 22
+      // Firefox
+      || e.code === 1014
+      // test name field too, because code might not be present
+      // everything except Firefox
+      || e.name === 'QuotaExceededError'
+      // Firefox
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      // acknowledge QuotaExceededError only if there's something already stored
+      && (storage && storage.length !== 0);
   }
 }
 
 function setItem(name, item) {
-  if(localStorageAvailable()) {
+  if (localStorageAvailable()) {
     localStorage.setItem(name, item);
   }
-};
+}
 
 function getItem(name) {
-  if(localStorageAvailable()) {
+  if (localStorageAvailable()) {
     return localStorage.getItem(name);
   }
-};
+}
 
 let books = getBooks() || [];
-if(books) {
+if (books) {
   displayBooks();
 }
 
 function saveBooks() {
   setItem('books', JSON.stringify(books));
-};
+}
 
 function getBooks() {
   return JSON.parse(getItem('books'));
-};
+}
 
 function addBook(title, author) {
   const id = books.length + 1;
   books.push({
-    id: id,
-    title: title,
-    author: author
+    id,
+    title,
+    author,
   });
 
   saveBooks();
   displayBooks();
-;};
+}
 
 function removeBook(bookId) {
-  books = books.filter((book) => { 
-    return book.id != bookId;
-  });
+  books = books.filter((book) => book.id != bookId);
 
   saveBooks();
   displayBooks();
-};
+}
 
 function displayBooks() {
   const booksDiv = document.querySelector('.books');
   booksDiv.textContent = '';
 
-  const createDiv = () => { return document.createElement('div'); };
+  const createDiv = () => document.createElement('div');
 
-  books.forEach((book, index) => {
+  books.forEach((book) => {
     const bookDiv = createDiv();
     bookDiv.className = 'book';
 
@@ -86,7 +83,7 @@ function displayBooks() {
 
     const removeBtn = document.createElement('button');
     removeBtn.className = ['removeBtn-', book.id].join('');
-    removeBtn.textContent = 'Remove'
+    removeBtn.textContent = 'Remove';
     removeBtn.addEventListener('click', (event) => {
       const source = event.target;
       const bookId = source.className.split('-')[1];
@@ -100,7 +97,7 @@ function displayBooks() {
 
     booksDiv.appendChild(bookDiv);
   });
-};
+}
 
 const addBtn = document.querySelector('#addBtn');
 addBtn.addEventListener('click', () => {
