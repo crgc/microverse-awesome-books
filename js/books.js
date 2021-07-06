be535383f734887fb3ec7d8a5b37b7cc43a61e11
Erick Contreras <eri89--@hotmail.com>
2021-07-06 17:12:37 -0500

    Fix Jslinters

--- js/books.js
+++ js/books.js
@@ -1,12 +1,13 @@
+/* eslint-disable max-classes-per-file */
 class StorageUtil {
-  constructor() {}
   localStorageAvailable() {
-    let storage;
+    this.storage = null;
+
     try {
-      storage = window.localStorage;
+      this.storage = window.localStorage;
       const x = '__storage_test__';
-      storage.setItem(x, x);
-      storage.removeItem(x);
+      this.storage.setItem(x, x);
+      this.storage.removeItem(x);
       return true;
     } catch (e) {
       return e instanceof DOMException && (
@@ -20,14 +21,16 @@ class StorageUtil {
         // Firefox
         || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
         // acknowledge QuotaExceededError only if there's something already stored
-        && (storage && storage.length !== 0);
+        && (this.storage && this.storage.length !== 0);
     }
   }
+
   setItem(name, item) {
     if (this.localStorageAvailable()) {
       localStorage.setItem(name, item);
     }
   }
+
   getItem(name) {
     if (this.localStorageAvailable()) {
       return localStorage.getItem(name);
@@ -50,18 +53,23 @@ class Library {
     this.storageUtil = new StorageUtil();
     this.init();
   }
+
   init() {
     this.books = JSON.parse(this.storageUtil.getItem('books')) || [];
   }
+
   save() {
     this.storageUtil.setItem('books', JSON.stringify(this.books));
   }
+
   addBook(title, author) {
     const id = this.books.length + 1;
     const book = new Book(id, title, author);
+
     this.books.push(book);
     this.save();
   }
+
   removeBook(bookId) {
     this.books = this.books.filter((book) => book.id != bookId); /* eslint-disable-line eqeqeq */
     this.save();
@@ -72,25 +80,33 @@ class Display {
   constructor(library) {
     this.library = library;
   }
+
   setRemoveButtonListener(removeButtonListener) {
     this.removeButtonListener = removeButtonListener;
   }
+
   render() {
     const booksDiv = document.querySelector('.books');
     booksDiv.textContent = '';
+
     const createDiv = () => document.createElement('div');
-    const books = this.library.books;
+    const { books } = this.library;
+
     books.forEach((book) => {
       const bookDiv = createDiv();
       bookDiv.className = 'book';
+
       const titleDiv = createDiv();
       titleDiv.textContent = book.title;
+
       const authorDiv = createDiv();
       authorDiv.textContent = book.author;
+
       const removeBtn = document.createElement('button');
       removeBtn.textContent = 'Remove';
       removeBtn.className = ['removeBtn-', book.id].join('');
       removeBtn.addEventListener('click', this.removeButtonListener);
+
       bookDiv.appendChild(titleDiv);
       bookDiv.appendChild(authorDiv);
       bookDiv.appendChild(removeBtn);
@@ -104,22 +120,27 @@ class App {
     this.library = new Library();
     this.display = new Display(this.library);
   }
+
   start() {
     const addBtn = document.querySelector('#addBtn');
     const thislibrary = this.library;
     const thisdisplay = this.display;
+
     addBtn.addEventListener('click', () => {
       const inputTitle = document.querySelector('.inputTitle');
       const inputAuthor = document.querySelector('.inputAuthor');
       thislibrary.addBook(inputTitle.value, inputAuthor.value);
       thisdisplay.render();
     });
-    const removeButtonListener = () => {
+
+    const removeButtonListener = (event) => {
       const source = event.target;
       const bookId = source.className.split('-')[1];
+
       thislibrary.removeBook(bookId);
       thisdisplay.render();
     };
+
     this.display.setRemoveButtonListener(removeButtonListener);
     this.display.render();
   }
@@ -127,3 +148,4 @@ class App {
 
 const app = new App();
 app.start();
+/* eslint-enable max-classes-per-file */
