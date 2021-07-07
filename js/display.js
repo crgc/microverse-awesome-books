d747c71a7dafda2fd00e7bb9460c152bc265cd11
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-07-07 16:32:15 -0400

    Fix ESLint errors

--- js/display.js
+++ js/display.js
@@ -15,18 +15,18 @@ export default class Display {
     const { books } = this.library;
 
     books.forEach((book, index) => {
-      const isEven = (n) => { return n % 2 == 0; };
+      const isEven = (n) => n % 2 === 0;
 
       const bookWrapperDiv = createDiv();
       bookWrapperDiv.className = isEven(index) ? 'book-wrapper bg-grey' : 'book-wrapper';
 
       const bookDiv = createDiv();
       bookDiv.className = 'book';
-      bookDiv.textContent = ["\"", book.title, "\"", ' by ', book.author].join('');
+      bookDiv.textContent = `"${book.title}" by ${book.author}`;
 
       const removeBtn = document.createElement('button');
       removeBtn.addEventListener('click', this.removeButtonListener);
-      removeBtn.className = ['removeBtn-', book.id].join('');
+      removeBtn.className = `remove-btn-${book.id}`;
       removeBtn.textContent = 'Remove';
 
       bookWrapperDiv.appendChild(bookDiv);
